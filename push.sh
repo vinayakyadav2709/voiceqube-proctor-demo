22dcca4bda896458d18615b5cb9d73b8696c44cc
falcon <vinayakyadav2709@gmail.com>
2026-02-19 07:57:21 +0530

    chore: remove unnecessary scripts and docs

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/falcon/Projects/Voiceqube/security-lib/packages/proctor
-
-echo "Pushing to your fork..."
-git push origin main
-
-echo ""
-echo "=== Push successful! ==="
-echo "Your fork: https://github.com/vinayakyadav2709/voiceqube-proctor"
